4360cfeb35665b09b02a5e8ad13407e13681c6f7
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-28 15:50:39 -0600

    Created form for adding comments to posts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,24 +5,33 @@ import { PostProvider } from "./posts/PostProvider"
 import { CategoryForm } from "./categories/CategoryForm"
 import { CategoryList } from "./categories/CategoryList"
 import { CategoryProvider } from "./categories/CategoryProvider"
+import { CommentProvider } from "./comments/CommentProvider"
+import { CommentForm } from "./comments/CommentForm"
 
 export const ApplicationViews = (props) => {
     return (
-    <>
-        <PostProvider>
-            <Route exact path = "/" render={
-                props => <PostList {...props} />
-            } />
-            <Route exact path = "/posts" render={
-                props => <PostList {...props} />
-            } />
-        </PostProvider>
-        <CategoryProvider>
-            <Route exact path="/categories">
-                <CategoryList {...props} />
-                <CategoryForm />
-            </Route>
-        </CategoryProvider>
- </>
+        <>
+            <CommentProvider>
+                <PostProvider>
+                    <Route exact path="/" render={
+                        props => <PostList {...props} />
+                    } />
+                    <Route exact path="/posts" render={
+                        props => <PostList {...props} />
+                    } />
+                </PostProvider>
+            </CommentProvider>
+            <CategoryProvider>
+                <Route exact path="/categories">
+                    <CategoryList {...props} />
+                    <CategoryForm />
+                </Route>
+            </CategoryProvider>
+            <CommentProvider>
+                <Route path="/addComment">
+                    <CommentForm {...props} />
+                </Route>
+            </CommentProvider>
+        </>
     )
 }
