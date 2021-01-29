af91e1c97b3ad480280bc5c728039ef8a198b61c
Mario Campopiano <mcampopiano92@gmail.com>
2021-01-29 10:18:09 -0600

    wrote functionality for comment list component

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -3,8 +3,19 @@ import { CommentContext } from "./CommentProvider"
 
 export const CommentList = props => {
     const {comments, getComments} = useContext(CommentContext)
+    const chosenPost = props.location.state.chosenPost
 
     useEffect(() => {
         getComments()
     }, [])
+
+    return (
+        <div className="commentList">
+            {
+                comments.map(comment => {
+                    return <Comment comment={comment} post={chosenPost} />
+                })
+            }
+        </div>
+    )
 }
