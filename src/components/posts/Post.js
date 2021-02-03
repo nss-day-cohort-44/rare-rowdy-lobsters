6c5cd9780fa039e8c0afc0b5fc5673e3853207f8
Your Github User Name <david.lav.williams@gmail.com>
2021-02-03 11:19:29 -0600

    admins can approve a users post

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,36 +1,51 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import { Link } from "react-router-dom"
 import { TagPost } from "../tags/TagPost"
+import { PostContext } from "./PostProvider"
 
-export const Post = ({post}) => {
-    const userType=parseInt(localStorage.getItem("userType"))
-    
-    function ShowApprovalButton(){
-        if (post.approved===0 && userType===1 ){
-            console.log("this should hit twice")
-            return(
-            <button>
-                approve?
-            </button>
+
+
+export const Post = ({ post }) => {
+    const userType = parseInt(localStorage.getItem("userType"))
+    const { updatePost } = useContext(PostContext)
+
+
+    function ShowApprovalButton() {
+        if (post.approved === 0 && userType === 1) {
+            return (
+                <button onClick={() => ApprovePost()}>
+                    approve?
+                </button>
             )
         }
-        else{
-            return(
+        else {
+            return (
                 <></>
             )
         }
-        }
+    }
 
-    
-    
+    function ApprovePost() {
+        const newPost = {
+            id: post.id,
+            user_id: post.user_id,
+            category_id: post.category_id,
+            title: post.title,
+            publication_date: Date.now(),
+            content: post.content,
+            approved: 1
+        }
+            updatePost(newPost)
+    }
+
     return (
-    <>
-    <div className="postCard">
-        <Link to={{pathname: `/posts/${post.id}`, state: {chosenPost: post}}}>Title: {post.title}</Link>
-        <p>Author: {post.user.first_name} {post.user.last_name}</p>
-        <p>Category: {post.category.label}</p>
-        <ShowApprovalButton></ShowApprovalButton>
-    </div>
-    </>
+        <>
+            <div className="postCard">
+                <Link to={{ pathname: `/posts/${post.id}`, state: { chosenPost: post } }}>Title: {post.title}</Link>
+                <p>Author: {post.user.first_name} {post.user.last_name}</p>
+                <p>Category: {post.category.label}</p>
+                <ShowApprovalButton ></ShowApprovalButton>
+            </div>
+        </>
     )
 }
