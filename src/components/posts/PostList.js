f02600bb09bdc3a8c1cfbb77273919f4dcff305b
Mario Campopiano <mcampopiano92@gmail.com>
2021-02-03 10:41:54 -0600

    added a dropdown menu that sorts posts by category

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,34 +1,62 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
+import { CategoryContext } from "../categories/CategoryProvider"
 import { Post } from "./Post"
 import { PostContext } from "./PostProvider"
 
 export const PostList = (props) => {
-    const {posts, getPosts} = useContext(PostContext)
+    const { posts, getPosts } = useContext(PostContext)
+    const { categories, getCategories } = useContext(CategoryContext)
+    const [filteredPosts, setFiltered] = useState([])
 
-    useEffect(() => {
-        getPosts()
-    }, [])
+    const changePosts = event => {
+        if (event.target.value !== "0") {
 
-    const reversedList = [...posts].reverse()
-    return (
-        <>
-        <button onClick={() => props.history.push("/createPost")}>
-                Add Post
-            </button>
-        {
-            
-            reversedList.map(post => {
-                if (props.location.pathname === "/posts" ) {
-                    return <Post post={post} />
+            const newPosts = []
+            posts.forEach(post => {
+                if (parseInt(post.category_id) === parseInt(event.target.value)) {
+                    newPosts.push(post)
                 }
-                
-                else if (parseInt(post.user_id) === parseInt(localStorage.getItem("rare_user_id"))) {
-                    return <Post post={post} />
-                }
-                // else
-                // return <h1>Boo!</h1>
+                setFiltered(newPosts)
             })
         }
+        else {
+            setFiltered(posts)
+        }
+    }
+
+    useEffect(() => {
+        setFiltered(posts)
+    }, [posts])
+    useEffect(() => {
+        getPosts().then(getCategories)
+    }, [])
+
+    const reversedList = [...filteredPosts].reverse()
+    return (
+        <>
+            <button onClick={() => props.history.push("/createPost")}>
+                Add Post
+            </button>
+            <select defaultValue="" onChange={changePosts}>
+                <option value="0">Filter by category</option>
+                {
+                    categories.map(cat => {
+                        return <option value={`${cat.id}`}>{cat.label}</option>
+                    })
+                }
+            </select>
+            {
+
+                reversedList.map(post => {
+                    if (props.location.pathname === "/posts") {
+                        return <Post post={post} />
+                    }
+
+                    else if (parseInt(post.user_id) === parseInt(localStorage.getItem("rare_user_id"))) {
+                        return <Post post={post} />
+                    }
+                })
+            }
         </>
     )
 }
